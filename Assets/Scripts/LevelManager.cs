4d1ef0cab8ef9a534e6497ee64abf55425225463
emalamela <lamelaemanuel@gmail.com>
2016-12-31 19:30:07 -0300

    Re-imported Starter pack

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,33 +4,63 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+	private const string LEVEL_SCENE_FORMAT_NAME = "level_{0:D2}";
+	private const string LOSE_SCENE_NAME = "lose";
+
+	private const string GAME_SCENE_NAME = "game";
+	private const string WIN_SCENE_NAME = "win";
+	private const string START_SCENE_NAME = "start";
+
 	public void loadGameScene() {
-		loadLevel("Game");
+		loadScene(GAME_SCENE_NAME);
 	}
 
 	public void loadWinScene() {
-		loadLevel("Win");
+		loadScene(WIN_SCENE_NAME);
 	}
 
 	public void loadLoseScene() {
-		loadLevel("Lose");
+		// TODO: Make a lose scene
+		loadScene(WIN_SCENE_NAME);
 	}
 
 	public void loadStartScene() {
-		loadLevel("Start");
+		loadScene(START_SCENE_NAME);
 	}
 
-	private void loadLevel(string levelName) {
+	private void loadScene(string sceneName) {
 		var activeSceneName = SceneManager.GetActiveScene().name;
-		if (activeSceneName.ToLower().Equals(levelName.ToLower())) {
+		if (activeSceneName.ToLower().Equals(sceneName.ToLower())) {
 			print("Scene with name " + activeSceneName + " is already loaded.");
 			return;
 		}
 
-		SceneManager.LoadScene(levelName);
+		SceneManager.LoadScene(sceneName);
 	}
 
 	public void quitGame() {
 		Application.Quit();
 	}
+
+	public void loadLevel(int level) {
+		loadScene(string.Format(LEVEL_SCENE_FORMAT_NAME, level));
+	}
+
+	public void loadNextLevel() {
+		int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+		int lastLevelSceneIndex = SceneManager.sceneCountInBuildSettings - 2;
+
+		if (sceneIndex >= lastLevelSceneIndex) {
+			loadWinScene();
+			return;
+		}
+
+		int firstLevelSceneIndex = 1;
+		if (sceneIndex < firstLevelSceneIndex) {
+			sceneIndex = firstLevelSceneIndex;
+		} 
+
+  		loadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+  	}
+
 }
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	private static MusicPlayer instance = null;
+     
+    public static MusicPlayer Instance {
+    	get { 
+        	return instance; 
+        }
+    }
+
+	private void Awake() {
+	    // If the singleton has been initialized, destroy the new instance 
+	    if (instance != null && instance != this) {
+	       Destroy(gameObject);
+	       return;
+	    }
+
+	    // Else, keep the instance
+	    instance = this;
+		// Prevent it from being destroyed when a scene change occurs
+	    DontDestroyOnLoad(gameObject);
+ 	}
+}
