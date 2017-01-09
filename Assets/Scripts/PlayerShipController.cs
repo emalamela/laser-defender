d33eb6ed94b88017b237d2cb5eefd3208f4459c9
emalamela <lamelaemanuel@gmail.com>
2017-01-08 23:24:09 -0300

    Added PlayerShip with horizontal velocity controlled through arrows

---
+++ Assets/Scripts/PlayerShipController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShipController : MonoBehaviour {
+
+    public float horizontalVelocity;
+
+    private new Rigidbody2D rigidbody;
+
+    private enum Movement {
+        LEFT = -1,
+        NONE = 0,
+        RIGHT = 1
+    }
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private static Movement GetMovementDirection() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            return Movement.LEFT;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            return Movement.RIGHT;
+        } else {
+            return Movement.NONE;
+        }
+    }
+
+    void FixedUpdate() {
+        HandleMovement();
+    }
+
+    private void HandleMovement() {
+        rigidbody.velocity = 
+            new Vector2(((int)GetMovementDirection()) * horizontalVelocity,
+                        rigidbody.velocity.y);
+    }
+
+}
