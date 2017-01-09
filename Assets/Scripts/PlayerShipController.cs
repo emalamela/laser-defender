1c511a6e76477bd70e48a09d47798fe7fbee572e
emalamela <lamelaemanuel@gmail.com>
2017-01-09 00:02:50 -0300

    Preventing PlayerShip out-of-camera movement

--- Assets/Scripts/PlayerShipController.cs
+++ Assets/Scripts/PlayerShipController.cs
@@ -8,6 +8,9 @@ public class PlayerShipController : MonoBehaviour {
 
     private new Rigidbody2D rigidbody;
 
+    private Vector3 leftBoundary;
+    private Vector3 rightBoundary;
+
     private enum Movement {
         LEFT = -1,
         NONE = 0,
@@ -16,6 +19,17 @@ public class PlayerShipController : MonoBehaviour {
 
     void Start() {
         rigidbody = GetComponent<Rigidbody2D>();
+
+        InitializeBoundaries();
+    }
+
+    private void InitializeBoundaries() {
+        Camera mainCamera = Camera.main;
+        Bounds rendererBounds = GetComponent<Renderer>().bounds;
+        leftBoundary = mainCamera.ViewportToWorldPoint(Vector3.zero);
+        leftBoundary += new Vector3(rendererBounds.extents.x + 0.2f, 0, 0);
+        rightBoundary = mainCamera.ViewportToWorldPoint(Vector3.right);
+        rightBoundary -= new Vector3(rendererBounds.extents.x + 0.2f, 0, 0);
     }
 
     private static Movement GetMovementDirection() {
@@ -33,9 +47,20 @@ public class PlayerShipController : MonoBehaviour {
     }
 
     private void HandleMovement() {
+        Movement movement = GetMovementDirection();
+
+        if (!CanMove(movement)) {
+            rigidbody.velocity = Vector2.zero;
+            return;
+        }
+
         rigidbody.velocity = 
-            new Vector2(((int)GetMovementDirection()) * horizontalVelocity,
-                        rigidbody.velocity.y);
+            new Vector2(((int) movement) * horizontalVelocity, rigidbody.velocity.y);
+    }
+
+    private bool CanMove(Movement movement) {
+        return !(leftBoundary.x >= transform.position.x && movement == Movement.LEFT) &&
+            !(rightBoundary.x <= transform.position.x && movement == Movement.RIGHT);
     }
 
 }
