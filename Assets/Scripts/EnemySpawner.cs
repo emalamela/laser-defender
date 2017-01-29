bea8102cea12181f427e8f398d3bc9bf9bec89c1
emalamela <lamelaemanuel@gmail.com>
2017-01-29 20:03:07 -0300

    Display bounding gizmo box around enemy formation

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,13 +6,42 @@ public class EnemySpawner : MonoBehaviour {
 
     public GameObject enemyPrefab;
 
+    public float gizmoBoxPadding = 2.0f;
+
 	void Start () {
-        int childCount = transform.childCount;
-        for (int i = 0; i < childCount; i++) {
-            Transform childTransform = transform.GetChild(i);
-            GameObject enemy = Instantiate(enemyPrefab, childTransform.position, Quaternion.identity) as GameObject;
-            enemy.transform.parent = childTransform;
-        }
+        SpawnEnemies();
 	}
+
+    void SpawnEnemies() {
+        foreach (Transform child in transform) {
+            GameObject enemy = Instantiate(enemyPrefab, child.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.DrawWireCube(
+            transform.position,
+            CalculateChildrenBounds().size + new Vector3(gizmoBoxPadding, gizmoBoxPadding, 0));
+    }
+
+    private Bounds CalculateChildrenBounds() {
+        Bounds childBounds = new Bounds(transform.position, Vector3.zero);
+
+        foreach (Transform child in transform) {
+            if (!childBounds.Contains(child.position)) {
+                childBounds.Expand(Abs(child.position));
+            }
+        }
+
+        return childBounds;
+    }
+
+    private Vector3 Abs(Vector3 target) {
+        return new Vector3(
+            Mathf.Abs(target.x),
+            Mathf.Abs(target.y),
+            Mathf.Abs(target.z));
+    }
 
 }
