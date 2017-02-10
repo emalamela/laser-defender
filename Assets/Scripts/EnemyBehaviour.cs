1d1a971aaea9f01ea25a67762ad2a8b1466535f2
emalamela <lamelaemanuel@gmail.com>
2017-02-10 00:20:24 -0300

    Laser hits, applies damage and destroys enemy ships

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public float health = 150.0f;
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        Laser collidingLaser = collider.GetComponent<Laser>();
+
+        if (collidingLaser == null) return;
+
+        collidingLaser.Hit();
+        health -= collidingLaser.getDamage();
+        if (health <= 0) Destroy(gameObject);
+    }
+	
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    public float damage = 100.0f;
+
+    public float getDamage() {
+        return damage;
+    }
+
+    public void Hit() {
+        Destroy(gameObject);
+    }
+
+}
